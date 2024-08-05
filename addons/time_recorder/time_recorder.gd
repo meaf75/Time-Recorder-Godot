b7f1491cd60a0b4399b273743ee768366f5a2204
meaf75 <meaf75@hotmail.com>
2024-08-05 01:01:18 -0500

    stable working

--- addons/time_recorder/time_recorder.gd
+++ addons/time_recorder/time_recorder.gd
@@ -1,21 +1,119 @@
 @tool
 extends EditorPlugin
+class_name TimeRecorder
 
-var popmenu
+class TRDay:
+	var date: int
+	var time_in_seconds: int
+
+	func to_dict() -> Dictionary:
+		return {
+			"date": date,
+			"time_in_seconds": time_in_seconds
+		}
+
+class TRMonth:
+	var month: int
+	var dates: Dictionary = {} # int,TRDay
+
+	func to_dict() -> Dictionary:
+		var dates_dict = {}
+
+		for key in dates.keys():
+			dates_dict[key] = dates[key].to_dict()
+
+		return {
+			"month": month,
+			"dates": dates_dict
+		}
+
+class TRYear:
+	var year: int
+	var months: Dictionary = {} # int,TRMonth
+
+	func to_dict() -> Dictionary:
+		var dict = {}
+
+		for key in months.keys():
+			dict[key] = months[key].to_dict()
+
+		return {
+			"year": year,
+			"months": dict
+		}
+
+class TRSession:
+	var years : Dictionary = {} # int,TRYear
+	var total_time_in_seconds : int = 0
+
+	func to_dict() -> Dictionary:
+		var dict = {}
+
+		for key in years.keys():
+			dict[key] = years[key].to_dict()
+
+		return {
+			"years": dict,
+			"total_time_in_seconds": total_time_in_seconds
+		}
+
+const TIME_RECODER_CONFIG_PATH = "user://time_recorder.cfg"
+const TIME_RECODER_SAVE_PATH = "res://addons/time_recorder/save_data.json"
 
 const menu_key = "editor/henlux"
 
+const CONFIG_CATEGORY = "CONFIG"
+const CONFIG_KEY_PAUSED = "paused"
+const CONFIG_KEY_NEXT_SAVE = "next_save"
+
+const SAVE_ON_SECONDS : int = 60 # 5 minutes
+
+var config_file : ConfigFile
 var time_recorder_window : TRWindow
 
+var is_paused : bool = false
+var session_name: String = "default"
+var next_save_time : float = 0
+
+var tr_sessions : Dictionary = { } # string, TRSession
+
+signal on_config_update
+signal on_save_data_update
+
 func _enter_tree():
+	config_file = ConfigFile.new()
+
+	if FileAccess.file_exists(TIME_RECODER_CONFIG_PATH):
+		config_file.load(TIME_RECODER_CONFIG_PATH)
+	else:
+		next_save_time = Time.get_unix_time_from_system()
+		config_file.set_value(CONFIG_CATEGORY, CONFIG_KEY_PAUSED, false)
+		config_file.set_value(CONFIG_CATEGORY, CONFIG_KEY_NEXT_SAVE, next_save_time)
+		config_file.save(TIME_RECODER_CONFIG_PATH)
+		tr_log("config file created")
+
+	is_paused = config_file.get_value(CONFIG_CATEGORY, CONFIG_KEY_PAUSED)
+
 	add_tool_menu_item("Time Recorder", open_window)
 	add_tool_menu_item("test_script", henlux)
 
+	if FileAccess.file_exists(TIME_RECODER_SAVE_PATH):
+		var file_saved_data = FileAccess.open(TIME_RECODER_SAVE_PATH, FileAccess.READ)
+		tr_sessions = deserialize_tr_sessions(file_saved_data.get_as_text())
+		file_saved_data.close()
+		tr_log("save data restored")
 
-	# Hide the main panel. Very much required.
-	_make_visible(false)
+	prepare_next_save()
+	set_process(!is_paused)
+
 
 func open_window():
+
+	if time_recorder_window:
+		time_recorder_window.move_to_foreground()
+		time_recorder_window.popup_centered()
+		return
+
 	var window_node = load("res://addons/time_recorder/time_recorder_window.tscn")
 	var instance = window_node.instantiate() as TRWindow
 	#window = MFHttpInspector.new()
@@ -23,43 +121,178 @@ func open_window():
 
 	add_child(instance)
 
-	print("opening windowx")
+	print("opening window")
 
 	time_recorder_window.popup_centered()
 	time_recorder_window.close_requested.connect(close_window)
-	time_recorder_window.open_window()
-	#window.show()
+	time_recorder_window.open_window(self)
 
-func henlux():
-	#var date
-	#date = Time.get_unix_time_from_datetime_string("2024-02-32")
-	#print(TRDateChecker.check_date_string("2024-02-32"))
-	print(Time.get_datetime_dict_from_datetime_string("2024-7-2",true))
-	#date = Time.get_unix_time_from_datetime_dict({
-		#year = 2024,
-		#month = 2,
-		#day =  32,
-		#hour = 0,
-		#minute = 0,
-		#second = 0
-	#})
-	#print(date)
-
-func _process(delta):
-	#print("henlo")
-	pass
 
 func close_window():
 	time_recorder_window.queue_free()
 	time_recorder_window = null
 	print("removing window")
 
-func _has_main_screen():
-	return true
 
-func _get_plugin_name():
-	return "Main Screen Plugin"
+func henlux():
+	#var date
+	#date = Time.get_unix_time_from_datetime_string("2024-02-32")
+	#print(TRDateChecker.check_date_string("2024-02-32"))
+	#var plugin_installation_path : String = get_script().get_path()
+	#plugin_installation_path = plugin_installation_path.replace("/time_recorder.gd", "")
+	#print(plugin_installation_path)
+	var pana  = { 1: "pollo"}
+	var date = Time.get_datetime_dict_from_system()
+	pana[date.day] = "blanco"
+
+	for key in pana:
+		print(typeof(key))
 
 
-func _get_plugin_icon():
-	return EditorInterface.get_editor_theme().get_icon("Node", "EditorIcons")
+# This function validates the time recorded in a session - year - month - day
+# the dictionary sessions is a reference so any provided dictionary will be updated
+func validate_tr_data(sessions: Dictionary, year_number: int, month_number: int, day_number: int = -1):
+
+	var session : TRSession
+	var year : TRYear
+	var month : TRMonth
+	var day : TRDay
+
+	var year_key = str(year_number)
+	var month_key = str(month_number)
+	var day_key = str(day_number)
+
+	# Get session
+	if sessions.has(session_name):
+		session = sessions[session_name]
+	else:
+		session = TRSession.new()
+		sessions[session_name] = session
+
+	# Get year
+	if session.years.has(year_key):
+		year = session.years[year_key]
+	else:
+		year = TRYear.new()
+		year.year = year_number
+		session.years[year_key] = year
+
+	# Get Month
+	if year.months.has(month_key):
+		month = year.months[month_key]
+	else:
+		month = TRMonth.new()
+		month.month = month_number
+		year.months[month_key] = month
+
+	# Get Day
+	if day_number != -1:
+		if month.dates.has(day_key):
+			day = month.dates[day_key]
+		else:
+			day = TRDay.new()
+			day.date = day_number
+			month.dates[day_key] = day
+
+
+func _process(delta):
+	if next_save_time < Time.get_unix_time_from_system():
+		save_time()
+
+
+func save_time():
+	var current_date = Time.get_datetime_dict_from_system()
+
+	tr_sessions.duplicate()
+
+	# Get session
+	validate_tr_data(
+		tr_sessions,
+		current_date.year,
+		current_date.month,
+		current_date.day
+	)
+
+	var session : TRSession = tr_sessions[session_name]
+	var year : TRYear = session.years[str(current_date.year)]
+	var month : TRMonth = year.months[str(current_date.month)]
+	var day : TRDay = month.dates[str(current_date.day)]
+
+	session.total_time_in_seconds += SAVE_ON_SECONDS
+	day.time_in_seconds += SAVE_ON_SECONDS
+
+	var file = FileAccess.open(TIME_RECODER_SAVE_PATH, FileAccess.WRITE)
+	file.store_string(JSON.stringify(serialize_tr_sessions(tr_sessions)))
+	file.close()
+
+	prepare_next_save()
+	tr_log("your develop time has been tracked")
+
+	on_save_data_update.emit()
+
+
+func prepare_next_save():
+	var next_save_time_updated = Time.get_unix_time_from_system()
+	next_save_time_updated += SAVE_ON_SECONDS
+	next_save_time = next_save_time_updated
+
+
+func serialize_tr_sessions(sessions : Dictionary) -> Variant:
+	var dict = {}
+
+	for key in sessions:
+		dict[key] = (sessions[key] as TRSession).to_dict()
+
+	return dict
+
+
+func deserialize_tr_sessions(json: String) -> Dictionary:
+	var file_saved_data = FileAccess.open(TIME_RECODER_SAVE_PATH, FileAccess.READ)
+	var data = JSON.parse_string(json)
+	var restored_data : Dictionary
+
+	file_saved_data.close()
+
+	for session_key in data:
+		var session = TRSession.new()
+		var json_session = data[session_key]
+		restored_data[session_key] = session
+
+		session.total_time_in_seconds = json_session.total_time_in_seconds
+
+		for year_key in json_session.years:
+			var year = TRYear.new()
+			var json_year = json_session.years[year_key]
+			session.years[year_key] = year
+
+			year.year = json_year.year
+
+			for month_key in json_year.months:
+				var month = TRMonth.new()
+				var json_month = json_year.months[month_key]
+				year.months[month_key] = month
+
+				month.month = month_key
+
+				for day_key in json_month.dates:
+					var day = TRDay.new()
+					var json_day = json_month.dates[day_key]
+					month.dates[day_key] = day
+
+					day.date = json_day.date
+					day.time_in_seconds = json_day.time_in_seconds
+
+	return restored_data
+
+func set_config(category: String, key: String, value: Variant):
+	config_file.set_value(category, key, value)
+	config_file.save(TIME_RECODER_CONFIG_PATH)
+
+	if category == CONFIG_CATEGORY:
+		if key == CONFIG_KEY_PAUSED:
+			is_paused = value
+
+	on_config_update.emit()
+
+func tr_log(message: String):
+	printraw("[TimeRecorder] %s\n" % message)
--- addons/time_recorder/time_recorder_window.gd
+++ addons/time_recorder/time_recorder_window.gd
@@ -9,6 +9,12 @@ var tr_days : Array[TRDayContainer]
 @export
 var label_active_month : Label
 
+@export
+var label_total_time : Label
+
+@export
+var button_pause : Button
+
 var active_month : int
 var active_year : int
 
@@ -18,20 +24,50 @@ var months = {
 	9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"
 }
 
+var _controller : TimeRecorder
+
 func _init():
 	var t = Time.get_datetime_dict_from_system()
-	print(t)
 
 
-func open_window():
+func open_window(controller: TimeRecorder):
+	_controller = controller
+
+	_controller.on_config_update.connect(refresh_window)
+	_controller.on_save_data_update.connect(refresh_window)
+
 	var current_date = Time.get_datetime_dict_from_system()
 	load_month(current_date.month,current_date.year)
 
+
 func load_month(month: int, year: int):
 
-	print("-------------------")
 	active_month = month
 	active_year = year
+
+	var sessions = _controller.tr_sessions.duplicate(true)
+
+	var year_key = str(year)
+	var month_key = str(month)
+
+	var session : TimeRecorder.TRSession
+	var tr_year : TimeRecorder.TRYear
+	var tr_month : TimeRecorder.TRMonth
+
+	if !sessions.has(_controller.session_name):
+		session = TimeRecorder.TRSession.new()
+	else:
+		session = sessions[_controller.session_name]
+
+	if !session.years.has(year_key):
+		tr_year = TimeRecorder.TRYear.new()
+	else:
+		tr_year = session.years[year_key]
+
+	if !tr_year.months.has(month_key):
+		tr_month = TimeRecorder.TRMonth.new()
+	else:
+		tr_month = tr_year.months[month_key]
 
 	var active_date = "%s %d" % [months[month], year]
 	var first_date = Time.get_datetime_dict_from_datetime_string(
@@ -46,8 +82,10 @@ func load_month(month: int, year: int):
 	if weekday == 0: # Sunday
 		weekday = 7
 
-
 	label_active_month.text = active_date
+	label_total_time.text = get_time_label_for_seconds(session.total_time_in_seconds)
+	button_pause.text = "Resume l>" if _controller.is_paused else "Pause [][]"
+
 	var active_days = 0
 
 	for i in tr_days.size():
@@ -58,14 +96,17 @@ func load_month(month: int, year: int):
 			active_days + 1
 		]
 
-		var tr_day : TRDayContainer = tr_days[i]
-		var styleBox: StyleBoxFlat = tr_day.get_theme_stylebox("panel").duplicate()
-		styleBox.set("bg_color", Color.BLUE)
-		tr_day.add_theme_stylebox_override("panel", styleBox)
+		var day_container : TRDayContainer = tr_days[i]
+		var styleBox: StyleBoxFlat = day_container.get_theme_stylebox("panel").duplicate()
 
-		if TRDateChecker.check_date_string(date_str) != OK or i + 1 < weekday:
-			print("invalid date %s, %d" % [date_str,i])
-			print(tr_day)
+		var label_day_number = ""
+		var label_time = ""
+		var is_a_day_of_active_month = TRDateChecker.check_date_string(date_str) != OK or i + 1 < weekday
+
+		styleBox.set("bg_color", Color.BLUE)
+		day_container.add_theme_stylebox_override("panel", styleBox)
+
+		if is_a_day_of_active_month:
 			var day_description = ""
 			var month_of_the_day = month
 
@@ -79,15 +120,35 @@ func load_month(month: int, year: int):
 			elif month_of_the_day < 1:
 				month_of_the_day = 12
 
-			tr_day.label_day_number.text = ""
-			tr_day.label_time.text = months[month_of_the_day]
-			tr_day.set_active_style(false)
-			continue
+			label_day_number = ""
+			label_time = months[month_of_the_day]
+		else:
+			active_days += 1
+			var day_key = str(active_days)
+			label_day_number = day_key
 
-		active_days += 1
-		tr_day.label_day_number.text = str(active_days)
-		tr_day.label_time.text = ""
-		tr_day.set_active_style(true)
+			if tr_month.dates.has(day_key):
+				var tr_day : TimeRecorder.TRDay = tr_month.dates[day_key]
+				label_time = get_time_label_for_seconds(tr_day.time_in_seconds)
+
+		day_container.set_active_style(is_a_day_of_active_month)
+		day_container.label_day_number.text = label_day_number
+		day_container.label_time.text = label_time
+
+
+func get_time_label_for_seconds(seconds: int) -> String:
+	var label_time : String = ""
+	var hours : int = int(seconds / 3600)
+
+	if hours >= 1:
+		var minutes_in_sec = seconds - hours * 3600
+		var minutes = int(minutes_in_sec / 60)
+		label_time = "%d h\n%d min" % [hours, minutes]
+	else:
+		var minutes = int(seconds / 60)
+		label_time = "%d min" %  minutes
+
+	return label_time
 
 
 func change_month(direction: int):
@@ -103,6 +164,14 @@ func change_month(direction: int):
 
 	load_month(next_month,next_year)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func refresh_window():
+	load_month(active_month, active_year)
+
+
+func _on_button_pause_pressed():
+	_controller.set_config(
+		TimeRecorder.CONFIG_CATEGORY,
+		TimeRecorder.CONFIG_KEY_PAUSED,
+		!_controller.is_paused
+	)
